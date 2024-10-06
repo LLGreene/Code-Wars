1c942d9549cee94814a9bd9ea20abc9c6039a40c
Lacey Greene <laceygreenenola@gmail.com>
2024-10-05 19:51:35 -0500

    Update vowelCount.js

--- vowelCount.js
+++ vowelCount.js
@@ -1,5 +1,11 @@
 // MOST EFFICIENT SOLUTION
 function getCount(str) {
+  return [...str.toLowerCase()].reduce((total, char) => total + (['a', 'e', 'i', 'o', 'u'].includes(char) ? 1 : 0), 0);
+}
+
+
+// SOLUTION 2
+function getCount(str) {
   // Create a string containing all applicable vowels
   const vowels = 'aeiou';
   // Initialize a starting sum to be compounded
