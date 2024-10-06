b34c2e36b3b5c1172e43a6540051c1ec3d704379
Lacey Greene <laceygreenenola@gmail.com>
2024-10-05 19:34:02 -0500

    Update multiples3or5.js
    add a more efficient solution using Array.from() method

--- multiples3or5.js
+++ multiples3or5.js
@@ -1,3 +1,10 @@
+// MOST EFFICIENT SOLUTION
+function solution(number) {
+  return Array.from({ length: number }, (_, i) => i)
+    .reduce((total, i) => (i % 3 === 0 || i % 5 === 0) ? total + i : total, 0);
+}
+
+// SOLUTION 2
 function solution(number){
   let total = 0;
   for (let i = 0; i < number; i++){
