e63b1a38ea4005678143efb655ee32b10a77ac21
Lacey Greene <laceygreenenola@gmail.com>
2024-10-05 19:25:57 -0500

    Update evenOrOdd.js
    Use a ternary conditional to minimize code.

--- evenOrOdd.js
+++ evenOrOdd.js
@@ -1,3 +1,9 @@
+// MOST EFFICIENT SOLUTION
+function evenOrOdd(number) {
+  return number % 2 === 0 ? "Even" : "Odd";
+}
+
+// SOLUTION 2
 function evenOrOdd(number) {
   if (number % 2 === 0){
     return "Even"
