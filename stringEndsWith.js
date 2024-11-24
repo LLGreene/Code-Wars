fdb83816f03eb0020bcf3d69bff08fa408d82217
Lacey Greene <laceygreenenola@gmail.com>
2024-11-23 23:35:15 -0600

    Update stringEndsWith.js

--- stringEndsWith.js
+++ stringEndsWith.js
@@ -1,7 +1,12 @@
+// Solution 1
+const solution = (strA, strB) => strA.endsWith(strB);
+
+//Solution 2
 function solution(str, ending){
   return str.endsWith(ending);
 }
 
+//Solution 3
 function solution(str1, str2){
   return str1.includes(str2) && str1.length - str2.length === str1.lastIndexOf(str2) ? true : false;
 }
